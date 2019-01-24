be0cbeefb1208a2bf24fda5895ccc3d2572b081c
Fahim Shariar <fahim.shoumik@gmail.com>
2019-01-24 14:36:55 +0600

    packages migrated to dart 2

--- lib/src/damerau.dart
+++ lib/src/damerau.dart
@@ -1,8 +1,7 @@
 // Copyright (c) 2016, Kwang Yul Seo. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:quiver_iterables/iterables.dart';
-
+import 'package:quiver/iterables.dart';
 import './base.dart';
 
 /// Implementation of Damerau-Levenshtein distance with transposition (also
